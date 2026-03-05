d867d008289e471c6ebb2d71c775d6d3acc37c4b
malek aziz hassayoun <malekaziz.hassayoun@esprit.tn>
2026-03-05 03:31:07 +0100

    correct the test input output

--- src/BlockGuard.Protection/obj/Debug/net9.0/BlockGuard.Protection.AssemblyInfo.cs
+++ src/BlockGuard.Protection/obj/Debug/net9.0/BlockGuard.Protection.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlockGuard.Protection")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c6f4812f930a344cd2be90bc58269faf13c55ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07775a26f1509fef9784a8974ebf824f4a74621")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlockGuard.Protection")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlockGuard.Protection")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
